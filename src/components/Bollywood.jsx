eda7a5eb160d712ebede879ce77081bffa9a3679
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-05-26 15:21:00 +0530

    Add files via upload

---
+++ src/components/Bollywood.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { happy } from "./APIs/Api";
+const Bollywood = () => {
+  const [data, setData] = useContext(happy);
+
+  return (
+    <>
+    <div className="contentPage">
+      
+      {data.map((item) => {
+        return (
+          <a href={item.url}>
+            <div className="card">
+              <img src={item.ImageAsset} alt="BollyImg" />
+              <p>{item.Title}</p>
+              <p>{item.BlogContent}</p>
+              <p>{item.PublishedDate}</p>
+              <hr />
+            </div></a>
+        );
+      })}
+      </div>
+      <div className="right">
+        
+      </div>
+    </>
+  );
+};
+
+export default Bollywood;
+
+
+
+
+
